36feb6b1bd1fb3e19de335d72d80af7ed52f79c1
ksenyay <ksenyausachova@gmail.com>
2025-11-17 09:50:42 +0200

    Add logs

--- product/src/products/middleware/require-auth.middleware.ts
+++ product/src/products/middleware/require-auth.middleware.ts
@@ -8,6 +8,8 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class RequireAuthMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    console.log(req.session);
+    console.log(req.currentUser);
     if (!req.currentUser) {
       throw new UnauthorizedException('Not authorized');
     }
