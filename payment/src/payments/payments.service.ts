2d1c6e513bea05cba29778921e2138ae983e1264
ksenyay <ksenyausachova@gmail.com>
2025-11-20 20:30:26 +0200

    Change redirect link

--- payment/src/payments/payments.service.ts
+++ payment/src/payments/payments.service.ts
@@ -43,8 +43,8 @@ export class PaymentsService {
       customer_email: email,
       client_reference_id: order.id,
       metadata: { orderId: order.id },
-      success_url: `https://soundio.vercel.app/checkout/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `https://soundio.vercel.app/checkout/cancel`,
+      success_url: `https://soundio-1.onrender.com/checkout/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `https://soundio-1.onrender.com/checkout/cancel`,
     });
 
     return { url: session.url };
