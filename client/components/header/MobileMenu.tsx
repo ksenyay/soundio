c00af6ca45557086ee64d605aba98d438cdc19f6
ksenyay <ksenyausachova@gmail.com>
2025-11-21 23:55:56 +0200

    Change to mobile header

--- client/components/header/MobileMenu.tsx
+++ client/components/header/MobileMenu.tsx
@@ -28,76 +28,45 @@ const MobileMenu = () => {
     fetchCurrentUser();
   }, []);
 
-  function signout() {
-    localStorage.removeItem("jwt");
-    setOpen(false);
-    window.location.href = "/";
-  }
-
   return (
-    <div className="md:hidden">
-      <button
-        onClick={() => setOpen(!open)}
-        className="fixed top-4 right-4 p-2 rounded-xl  hover:bg-white/20 transition-all backdrop-blur-xl z-[1000]"
-      >
-        {open ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
-      </button>
+    currentUser && (
+      <div className="md:hidden">
+        <button
+          onClick={() => setOpen(!open)}
+          className="fixed top-4 right-4 p-2 rounded-xl  hover:bg-white/20 transition-all backdrop-blur-xl z-[1000]"
+        >
+          {open ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
+        </button>
 
-      <div
-        className={clsx(
-          "fixed top-19 left-0 w-full z-40 border-t border-white/10 " +
-            "bg-black/10 shadow-xl overflow-hidden backdrop-blur-xl " +
-            "transition-all duration-300 origin-top border-1 border-white",
-          open
-            ? "max-h-96 opacity-100 translate-y-0"
-            : "max-h-0 opacity-0 -translate-y-2 pointer-events-none"
-        )}
-      >
-        <nav className="flex flex-col py-2 text-md font-medium text-foreground">
-          {currentUser && (
-            <p className="px-6 py-3 hover:bg-white/10 text-lg transition-colors border-b-1 border-white/50">
-              {currentUser.username}
-            </p>
+        <div
+          className={clsx(
+            "fixed top-19 left-0 w-full z-40 border-t border-white/10 " +
+              "bg-black/10 shadow-xl overflow-hidden backdrop-blur-xl " +
+              "transition-all duration-300 origin-top border-1 border-white",
+            open
+              ? "max-h-96 opacity-100 translate-y-0"
+              : "max-h-0 opacity-0 -translate-y-2 pointer-events-none"
           )}
-
-          {currentUser ? (
-            <>
-              {/* <Link
-                href="/profile"
-                onClick={() => setOpen(false)}
-                className="px-6 py-4 hover:bg-white/10 transition-colors border-b-1 border-white/10"
-              >
-                Profile {}
-              </Link>
-
-              <button
-                onClick={signout}
-                className="px-6 py-4 text-left hover:bg-white/10 transition-colors  text-red-400"
-              >
-                Sign out
-              </button> */}
-            </>
-          ) : (
-            <>
-              <Link
-                href="/auth/login"
-                onClick={() => setOpen(false)}
-                className="px-6 py-4 hover:bg-white/10 transition-colors border-b-1 border-white/10"
-              >
-                Login
-              </Link>
-              <Link
-                href="/auth/signup"
-                onClick={() => setOpen(false)}
-                className="px-6 py-3 hover:bg-white/10 transition-colors"
-              >
-                Sign up
-              </Link>
-            </>
-          )}
-        </nav>
+        >
+          <nav className="flex flex-col py-2 text-md font-medium text-foreground">
+            <Link
+              href="/auth/login"
+              onClick={() => setOpen(false)}
+              className="px-6 py-4 hover:bg-white/10 transition-colors border-b-1 border-white/10"
+            >
+              Login
+            </Link>
+            <Link
+              href="/auth/signup"
+              onClick={() => setOpen(false)}
+              className="px-6 py-3 hover:bg-white/10 transition-colors"
+            >
+              Sign up
+            </Link>
+          </nav>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
